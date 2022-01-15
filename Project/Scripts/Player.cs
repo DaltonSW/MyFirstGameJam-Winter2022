40dd428e92230578d9176751a5dbf52c99dd882e
Dalton <daltonstwilliams@gmail.com>
2022-01-15 11:18:53 -0600

    -Test SMB platform. 2 player scenes, Kinematic and Rigid

--- Project/Scripts/Player.cs
+++ Project/Scripts/Player.cs
@@ -3,35 +3,38 @@ using System;
 
 public class Player : KinematicBody2D
 {
+	private float MOVE_AMT = 2;
+	private float GRAVITY = 1;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		
 	}
 
- // Called every frame. 'delta' is the elapsed time since the previous frame.
+ 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
-		Console.Write(delta);
-		float movementAmount = 5;
+		MoveAndCollide(new Vector2(0, GRAVITY));
+
 		if (Input.IsKeyPressed((int)KeyList.W))
 		{
-			MoveAndCollide(new Vector2(0, -movementAmount));
+			MoveAndCollide(new Vector2(0, -MOVE_AMT));
 		}
 
 		if (Input.IsKeyPressed((int)KeyList.S))
 		{
-			MoveAndCollide(new Vector2(0, movementAmount));
+			MoveAndCollide(new Vector2(0, MOVE_AMT));
 		}
 
 		if (Input.IsKeyPressed((int)KeyList.D))
 		{
-			MoveAndCollide(new Vector2(movementAmount, 0));
+			MoveAndCollide(new Vector2(MOVE_AMT, 0));
 		}
 
 		if (Input.IsKeyPressed((int)KeyList.A))
 		{
-			MoveAndCollide(new Vector2(-movementAmount, 0));
+			MoveAndCollide(new Vector2(-MOVE_AMT, 0));
 		}
 	}
 }
