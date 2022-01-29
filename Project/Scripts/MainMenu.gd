5541f5996f8cd524fccdf2bb395bfec33b414066
Dalton <daltonstwilliams@gmail.com>
2022-01-29 09:53:25 -0600

    Moved MainMenu script into Scripts folder

--- Project/Scripts/MainMenu.gd
+++ Project/Scripts/MainMenu.gd
@@ -17,7 +17,7 @@ func _ready():
 
 
 func _on_StartButton_pressed():
-	get_tree().change_scene("res://Scenes/Test_Scene_1.tscn")
+	get_tree().change_scene("res://Scenes/LevelHolder.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
