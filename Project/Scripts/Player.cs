2f82739fbf7be663c5f8ee3e83af15906ada887c
Dalton <daltonstwilliams@gmail.com>
2022-01-30 00:28:13 -0600

    Shotgun should autoequip on shoot now

--- Project/Scripts/Player.cs
+++ Project/Scripts/Player.cs
@@ -356,8 +356,12 @@ public class Player : KinematicBody2D
 				animatedSprite.Play("run");
 			}
 
-			if (Input.IsActionJustPressed("player_shoot") && IS_SHOTGUN_EQUIPPED && (CUR_SHOTGUN_BUFFER == 0))
+			if (Input.IsActionJustPressed("player_shoot") && (CUR_SHOTGUN_BUFFER == 0))
 			{
+				if (!IS_SHOTGUN_EQUIPPED)
+				{
+					EquipShotgun();
+				}
 				CUR_SHOTGUN_BUFFER = delta;
 				ShootShotgun();
 			}
